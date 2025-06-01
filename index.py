b698416c9ea0777a8e7ed7993ce12c69e38f23f5
shreygupta04 <shreygupta04@gmail.com>
2025-06-01 13:57:38 -0400

    templates import

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ from scrape_runner import run_scrape
 from scripts.progress import get_progress
 from threading import Thread
 
-app = Flask(__name__, template_folder="../templates")
+app = Flask(__name__, template_folder="templates")
 
 @app.route("/", methods=["GET"])
 def index():
